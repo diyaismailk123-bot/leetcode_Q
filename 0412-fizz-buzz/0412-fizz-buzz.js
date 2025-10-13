5ecfb3730d6c8a9c398a35c2a32fdd9b36c168fa
Diya_fathima <diyaismailk123@gmail.com>
2025-10-13 16:47:15 +0530

    Time: 0 ms (100%), Space: 58.1 MB (51.89%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let x=[]
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+             x.push("FizzBuzz")
+        } else if (i % 3 === 0) {
+             x.push("Fizz")
+        } else if (i % 5 === 0) {
+            x.push("Buzz")
+        } else {
+          x.push(i.toString());
+        }
+
+    }
+        return x
+};
